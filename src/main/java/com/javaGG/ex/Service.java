41d06c36caa6bbbbbb6951629952021ba1f8a74f
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-10-06 15:56:50 +0900

    commandEx

--- src/main/java/com/javaGG/ex/Service.java
+++ src/main/java/com/javaGG/ex/Service.java
@@ -6,5 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Service {
+	
 	public ArrayList<MemberDTO> excute(HttpServletRequest request, HttpServletResponse response);
+
 }
